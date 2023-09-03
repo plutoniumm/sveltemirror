a2b828f617e472fe3ff6e35e954510d23b4054fc
plutoniumm <51614345+plutoniumm@users.noreply.github.com>
2023-09-03 06:08:04 +0530

    home is demo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
     import CodeMirror from "$lib";
-    import { javascript } from "@codemirror/lang-javascript";
+    import { html } from "@codemirror/lang-html";
     import { oneDark } from "@codemirror/theme-one-dark";
+    import { onMount } from "svelte";
+    import Template from "./template.html?raw";
+    import { base } from "$app/paths";
 
-    let value = "";
+    let //
+        value = "",
+        old = "",
+        doc = null as any;
 
     let props: CodeMirror["$$prop_def"] = {
         basic: true,
@@ -13,25 +19,89 @@
         readonly: false,
         tabSize: 2,
         placeholder: null,
-        lang: javascript({ typescript: true }),
+        lang: html(),
         theme: oneDark,
+    };
+
+    const write = (text: any) => {
+        doc.open();
+        doc.write(text);
+        doc.close();
+    };
+
+    const handleChange = () => {
+        const current = value;
+        if (old === current) return 0;
+
+        old = current;
+        write(current);
+    };
+
+    onMount(() => {
+        value = Template;
+        if (!doc)
+            doc = document.getElementById("mfWHAT").contentWindow.document;
+        write(value);
+    });
+
+    const handleMessage = ({ data }) => {
+        if (data.type !== "code") return 0; // ignore if not code
+
+        const { code } = data;
+        value = code;
+        write(code);
     };
 </script>
 
-<div class="demo">
-    <CodeMirror bind:value class="editor" {...props} />
-</div>
+<svelte:window on:message={handleMessage} />
+
+<main class="f">
+    <div class="editor">
+        <CodeMirror
+            bind:value
+            class="editor"
+            styles={{
+                "&": {
+                    fontSize: "18px",
+                    height: "100%",
+                    width: "100%",
+                },
+            }}
+            {...props}
+            on:change={handleChange}
+        />
+    </div>
+    <iframe
+        id="mfWHAT"
+        frameborder="0"
+        title="Editor Output"
+        style="background: #fff;"
+    />
+</main>
 
 <style>
-    .demo {
-        position: relative;
-        height: 100%;
-        display: grid;
-        grid-template-rows: 1fr auto;
-        overflow: hidden;
+    main {
+        display: flex;
+        height: 100vh;
+        width: 100vw;
+        background: #eef;
     }
 
-    :global(.editor) {
-        overflow: hidden;
+    .editor,
+    iframe {
+        width: 50%;
+        height: 100%;
+    }
+
+    @media (max-width: 768px) {
+        main {
+            flex-direction: column;
+        }
+
+        .editor,
+        iframe {
+            width: 100%;
+            height: 50%;
+        }
     }
 </style>
--- svelte.config.js
+++ svelte.config.js
@@ -6,10 +6,7 @@ const config = {
     preprocess: preprocess(),
 
     kit: {
-        adapter: adapter( {
-            pages: "build",
-            assets: "build"
-        } ),
+        adapter: adapter(),
         paths: {
             base: "/sveltemirror",
         },
