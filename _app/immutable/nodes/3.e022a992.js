dbf905ec774f8badf8494f44f014d3d6a1f786f0
plutoniumm <51614345+plutoniumm@users.noreply.github.com>
2023-09-04 02:39:26 +0000

    Deploying to gh-pages from @ plutoniumm/sveltemirror@65d24dba42a90b8c875e185832805ca353ee0816 🚀

---
+++ _app/immutable/nodes/3.e022a992.js
@@ -0,0 +1,75 @@
+import{s as Re,f as a,a as l,l as fe,B as Ee,g as s,C as m,d as B,c as o,h as Pe,m as ue,j as Se,k as je,x as n,i as De,y as qe}from"../chunks/scheduler.d5f951d9.js";import{S as Ie,i as ze,b as C,d as _,m as y,a as b,t as k,e as H}from"../chunks/index.852cda47.js";import{C as N,o as F,h as U,R as Be}from"../chunks/index.67a2fe99.js";function Ne(i){let h,ce=`body {
+      background: #eef;
+    }`,G,e,M,ve="Sveltemirror 📚",V,T,ge=`This is a collection of Svelte components that use
+    <a href="https://codemirror.net/">CodeMirror</a> to provide code editing functionality.`,W,w,$e="Getting Started",Y,L,xe="To get started, install the package from npm:",K,p,Ce='<code class="language-bash">npm install sveltemirror</code>',Q,P,_e="Usage",X,S,ye="Basic",Z,f,ee,u,te,j,be="Styling",ne,D,ke=`The editor can accept props to be passed as styles to the editor and also
+    custom themes`,re,c,le,v,oe,R,He="Renderer",ae,E,Me=`For ease of use a barebones iframe renderer is provided. The renderer
+    accepts an optional string preprocess function that can be used to modify
+    the code before it is rendered.`,se,g,ie,d,$,me,x,he,q,Te="<br/>",de,I,we="<br/>",pe,z,Le="<br/>",J;return f=new N({props:{readonly:!0,class:"rx10 m10",theme:F,value:`<script>
+    import CodeMirror from "sveltemirror";
+    import { html } from "@codemirror/lang-html";
+
+    let value = "${i[0].html}";
+<\/script>
+
+<CodeMirror bind:value lang={html()} />`}}),u=new N({props:{lang:U(),value:i[0].html}}),c=new N({props:{readonly:!0,class:"rx10 m10",theme:F,value:`<script>
+    import CodeMirror from "sveltemirror";
+    import { html } from "@codemirror/lang-html";
+    import { oneDark } from "@codemirror/theme-one-dark";
+
+    let value = "${i[0].html}";
+<\/script>
+
+  <CodeMirror
+    bind:value
+    lang={html()}
+    theme={oneDark}
+    styles={${JSON.stringify(i[0].style,null,2).split(`
+`).map(Ae).join(`
+`).trim()}}
+  />`}}),v=new N({props:{lang:U(),theme:F,styles:i[0].style,value:i[0].html}}),g=new N({props:{readonly:!0,class:"rx10 m10",theme:F,value:`<script>
+    import CodeMirror, { Renderer } from "sveltemirror";
+    import { html } from "@codemirror/lang-html";
+    import { oneDark } from "@codemirror/theme-one-dark";
+
+    let value = "${i[0].html}";
+  <\/script>
+
+    <div style="display:flex;"
+      <CodeMirror
+        bind:value
+        theme={oneDark}
+        styles={{
+          "&": {
+            height: "150px",
+          },
+        }}
+        lang={html()}
+      />
+
+      <Renderer
+        bind:value
+        preprocess={(v) => "Hiiiii" + v}
+      />
+    </div>`}}),$=new N({props:{lang:U(),theme:F,value:i[0].html,styles:{"&":{height:"150px"}}}}),x=new Be({props:{lang:U(),value:i[0].html,preprocess:Fe}}),{c(){h=a("style"),h.textContent=ce,G=l(),e=a("main"),M=a("h1"),M.textContent=ve,V=l(),T=a("p"),T.innerHTML=ge,W=l(),w=a("h2"),w.textContent=$e,Y=l(),L=a("p"),L.textContent=xe,K=l(),p=a("pre"),p.innerHTML=Ce,Q=l(),P=a("h2"),P.textContent=_e,X=l(),S=a("h3"),S.textContent=ye,Z=fe(`
+  The most basic usage for just the editor is as follows:
+
+  
+  `),C(f.$$.fragment),ee=fe(`
+
+  It should create an editor that looks like this:
+
+  `),C(u.$$.fragment),te=l(),j=a("h3"),j.textContent=be,ne=l(),D=a("p"),D.textContent=ke,re=l(),C(c.$$.fragment),le=fe(`
+
+  this will create an editor that looks like this:
+  `),C(v.$$.fragment),oe=l(),R=a("h2"),R.textContent=He,ae=l(),E=a("p"),E.textContent=Me,se=l(),C(g.$$.fragment),ie=l(),d=a("div"),C($.$$.fragment),me=l(),C(x.$$.fragment),he=l(),q=a("p"),q.innerHTML=Te,de=l(),I=a("p"),I.innerHTML=we,pe=l(),z=a("p"),z.innerHTML=Le,this.h()},l(r){const A=Ee("svelte-zpasi5",document.head);h=s(A,"STYLE",{"data-svelte-h":!0}),m(h)!=="svelte-1geob70"&&(h.textContent=ce),A.forEach(B),G=o(r),e=s(r,"MAIN",{class:!0,style:!0});var t=Pe(e);M=s(t,"H1",{"data-svelte-h":!0}),m(M)!=="svelte-1gc0q03"&&(M.textContent=ve),V=o(t),T=s(t,"P",{"data-svelte-h":!0}),m(T)!=="svelte-11e6mmd"&&(T.innerHTML=ge),W=o(t),w=s(t,"H2",{"data-svelte-h":!0}),m(w)!=="svelte-1ris1ql"&&(w.textContent=$e),Y=o(t),L=s(t,"P",{"data-svelte-h":!0}),m(L)!=="svelte-13gf789"&&(L.textContent=xe),K=o(t),p=s(t,"PRE",{class:!0,"data-svelte-h":!0}),m(p)!=="svelte-ptdm6x"&&(p.innerHTML=Ce),Q=o(t),P=s(t,"H2",{"data-svelte-h":!0}),m(P)!=="svelte-dnbfhr"&&(P.textContent=_e),X=o(t),S=s(t,"H3",{"data-svelte-h":!0}),m(S)!=="svelte-g9rhso"&&(S.textContent=ye),Z=ue(t,`
+  The most basic usage for just the editor is as follows:
+
+  
+  `),_(f.$$.fragment,t),ee=ue(t,`
+
+  It should create an editor that looks like this:
+
+  `),_(u.$$.fragment,t),te=o(t),j=s(t,"H3",{"data-svelte-h":!0}),m(j)!=="svelte-qoco7w"&&(j.textContent=be),ne=o(t),D=s(t,"P",{"data-svelte-h":!0}),m(D)!=="svelte-1cu6me4"&&(D.textContent=ke),re=o(t),_(c.$$.fragment,t),le=ue(t,`
+
+  this will create an editor that looks like this:
+  `),_(v.$$.fragment,t),oe=o(t),R=s(t,"H2",{"data-svelte-h":!0}),m(R)!=="svelte-9l2j21"&&(R.textContent=He),ae=o(t),E=s(t,"P",{"data-svelte-h":!0}),m(E)!=="svelte-1mz6swx"&&(E.textContent=Me),se=o(t),_(g.$$.fragment,t),ie=o(t),d=s(t,"DIV",{style:!0});var O=Pe(d);_($.$$.fragment,O),me=o(O),_(x.$$.fragment,O),O.forEach(B),he=o(t),q=s(t,"P",{"data-svelte-h":!0}),m(q)!=="svelte-pntvnr"&&(q.innerHTML=Te),de=o(t),I=s(t,"P",{"data-svelte-h":!0}),m(I)!=="svelte-pntvnr"&&(I.innerHTML=we),pe=o(t),z=s(t,"P",{"data-svelte-h":!0}),m(z)!=="svelte-pntvnr"&&(z.innerHTML=Le),t.forEach(B),this.h()},h(){Se(p,"class","svelte-98j2lk"),je(d,"display","flex"),Se(e,"class","mx-a"),je(e,"width","min(100%, 600px)")},m(r,A){n(document.head,h),De(r,G,A),De(r,e,A),n(e,M),n(e,V),n(e,T),n(e,W),n(e,w),n(e,Y),n(e,L),n(e,K),n(e,p),n(e,Q),n(e,P),n(e,X),n(e,S),n(e,Z),y(f,e,null),n(e,ee),y(u,e,null),n(e,te),n(e,j),n(e,ne),n(e,D),n(e,re),y(c,e,null),n(e,le),y(v,e,null),n(e,oe),n(e,R),n(e,ae),n(e,E),n(e,se),y(g,e,null),n(e,ie),n(e,d),y($,d,null),n(d,me),y(x,d,null),n(e,he),n(e,q),n(e,de),n(e,I),n(e,pe),n(e,z),J=!0},p:qe,i(r){J||(b(f.$$.fragment,r),b(u.$$.fragment,r),b(c.$$.fragment,r),b(v.$$.fragment,r),b(g.$$.fragment,r),b($.$$.fragment,r),b(x.$$.fragment,r),J=!0)},o(r){k(f.$$.fragment,r),k(u.$$.fragment,r),k(c.$$.fragment,r),k(v.$$.fragment,r),k(g.$$.fragment,r),k($.$$.fragment,r),k(x.$$.fragment,r),J=!1},d(r){r&&(B(G),B(e)),B(h),H(f),H(u),H(c),H(v),H(g),H($),H(x)}}}const Ae=i=>"	"+i,Fe=i=>"Hiiiii"+i;function Ge(i){return[{html:"<h1> Hi there! </h1>",style:{"&":{width:"500px",maxWidth:"100%",height:"100px"}}}]}class Ve extends Ie{constructor(h){super(),ze(this,h,Ge,Ne,Re,{})}}export{Ve as component};
